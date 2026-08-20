60864eef39c5402d6ef731359dfe680d47abaeba
Nikola <nikolapan53@gmail.com>
2026-08-20 13:29:10 +0200

    Base Crud Repository

---
+++ src/BuildingBlocks/FitTracker.BuildingBlocks.Infrastrucutre/Database/CrudDatabaseRepository.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FitTracker.BuildingBlocks.Core.Domain;
+using FitTracker.BuildingBlocks.Core.UseCases;
+
+namespace FitTracker.BuildingBlocks.Infrastrucutre.Database;
+
+public class CrudDatabaseRepository<TEntity, TDbContext> : ICrudRepository<TEntity>
+    where TEntity : Entity
+    where TDbContext : DbContext
+{
+    protected readonly TDbContext DbContext;
+    private readonly DbSet<TEntity> _dbSet;
+
+    public CrudDatabaseRepository(TDbContext dbContext)
+    {
+        DbContext = dbContext;
+        _dbSet = DbContext.Set<TEntity>();
+    }
+
+    public TEntity? Get(int id)
+    {
+        return _dbSet.Find(id);
+    }
+
+    public List<TEntity> GetMany(List<int> ids)
+    {
+        return _dbSet.Where(e => ids.Contains(e.Id)).ToList();
+    }
+
+    public PagedResult<TEntity> GetPaged(int page, int pageSize)
+    {
+        var task = _dbSet.GetPagedById(page, pageSize);
+        task.Wait();
+        return task.Result;
+    }
+
+    public TEntity Create(TEntity entity)
+    {
+        _dbSet.Add(entity);
+        return entity;
+    }
+
+    public List<TEntity> CreateMany(List<TEntity> entities)
+    {
+        _dbSet.AddRange(entities);
+        return entities;
+    }
+
+    public TEntity Update(TEntity entity)
+    {
+        _dbSet.Update(entity);
+        return entity;
+    }
+
+    public void Delete(TEntity entity)
+    {
+        _dbSet.Remove(entity);
+    }
+}
---
+++ src/BuildingBlocks/FitTracker.BuildingBlocks.Infrastrucutre/Database/PagedExtension.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FitTracker.BuildingBlocks.Core.UseCases;
+using FitTracker.BuildingBlocks.Core.Domain;
+
+
+namespace FitTracker.BuildingBlocks.Infrastrucutre.Database;
+public static class PagedExtension
+{
+    public static async Task<PagedResult<T>> GetPagedById<T>(this IQueryable<T> source, int pageIndex, int pageSize) where T : Entity
+    {
+        var count = await source.CountAsync();
+
+        if (pageSize != 0 && pageIndex != 0)
+        {
+            source = source.OrderBy(x => x.Id).Skip((pageIndex - 1) * pageSize).Take(pageSize);
+        }
+
+        var items = await source.ToListAsync();
+        return new PagedResult<T>(items, count);
+    }
+}
