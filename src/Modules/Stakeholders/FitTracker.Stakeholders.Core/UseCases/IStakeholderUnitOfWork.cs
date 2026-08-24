bafa253273a906020007914dd8895d65691dee21
Nikola <nikolapan53@gmail.com>
2026-08-24 12:15:11 +0200

    Stakeholder UoW

---
+++ src/Modules/Stakeholders/FitTracker.Stakeholders.Core/UseCases/IStakeholderUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿using FitTracker.BuildingBlocks.Core.UseCases;
+
+namespace FitTracker.Stakeholders.Core.UseCases
+{
+    public interface IStakeholderUnitOfWork : IUnitOfWork
+    {
+    }
+}
---
+++ src/Modules/Stakeholders/FitTracker.Stakeholders.Infrastructure/Database/StakeholderUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using FitTracker.BuildingBlocks.Infrastrucutre.Database;
+using FitTracker.Stakeholders.Core.UseCases;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitTracker.Stakeholders.Infrastructure.Database
+{
+    public class StakeholderUnitOfWork : UnitOfWork<StakeholderContext>, IStakeholderUnitOfWork
+    {
+        public StakeholderUnitOfWork(StakeholderContext context) : base(context)
+        {
+        }
+    }
+}
