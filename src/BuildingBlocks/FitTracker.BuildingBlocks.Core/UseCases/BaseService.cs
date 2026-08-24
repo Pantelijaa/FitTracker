1decff87ee46f3eb0b8a253d6f914b7cc864f584
Nikola <nikolapan53@gmail.com>
2026-08-24 12:36:01 +0200

    Base Service

---
+++ src/BuildingBlocks/FitTracker.BuildingBlocks.Core/UseCases/BaseService.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using FitTracker.BuildingBlocks.Core.Domain;
+using FluentResults;
+
+namespace FitTracker.BuildingBlocks.Core.UseCases
+{
+    public abstract class BaseService<TDomain, TDto> where TDomain : Entity
+    {
+        private readonly IMapper _mapper;
+
+        protected BaseService(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        protected TDomain MapToDomain(TDto dto)
+        {
+            return _mapper.Map<TDomain>(dto);
+        }
+
+        protected List<TDomain> MapToDomain(List<TDto> dtos)
+        {
+            return dtos.Select(dto => _mapper.Map<TDomain>(dto)).ToList();
+        }
+
+        protected TDto MapToDto(TDomain domain)
+        {
+            return _mapper.Map<TDto>(domain);
+        }
+
+        protected Result<List<TDto>> MapToDto(Result<List<TDomain>> domains)
+        {
+            if (domains.IsFailed)
+                return Result.Fail<List<TDto>>(domains.Errors);
+
+            return domains.Value.Select(_mapper.Map<TDto>).ToList();
+        }
+
+        protected Result<PagedResult<TDto>> MapToDto(Result<PagedResult<TDomain>> domains)
+        {
+            if (domains.IsFailed)
+                return Result.Fail<PagedResult<TDto>>(domains.Errors);
+
+            var items = domains.Value.Result.Select(_mapper.Map<TDto>).ToList();
+            return new PagedResult<TDto>(items, domains.Value.TotalCount);
+        }
+    }
+}
---
+++ src/Modules/Stakeholders/FitTracker.Stakeholders.Infrastructure/Database/Repositories/TraineeDatabaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using FitTracker.BuildingBlocks.Infrastrucutre.Database;
+using FitTracker.Stakeholders.Core.Domain;
+using FitTracker.Stakeholders.Core.Domain.RepositoryInterfaces;
+
+namespace FitTracker.Stakeholders.Infrastructure.Database.Repositories
+{
+    public class TraineeDatabaseRepository : CrudDatabaseRepository<Trainee, StakeholdersContext>, ITraineeRepository
+    {
+        public TraineeDatabaseRepository(StakeholdersContext context) : base(context)
+        {
+        }
+    }
+}
