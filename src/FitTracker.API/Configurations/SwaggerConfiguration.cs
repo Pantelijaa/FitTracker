8a90ba40e2ed30e66369c7be9d3ca73f886bbc08
Nikola <nikolapan53@gmail.com>
2026-08-24 17:31:53 +0200

    ASP configured

---
+++ src/FitTracker.API/Configurations/SwaggerConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.OpenApi;
+
+namespace FitTracker.API.Configurations
+{
+    public static class SwaggerConfiguration
+    {
+        public static IServiceCollection ConfigureSwagger(this IServiceCollection services, IConfiguration configuration)
+        {
+            var contactAdress = configuration.GetValue<string>("url");
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "FitTracker API",
+                    Version = "v1",
+                    Description = "API for FitTracker application",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "FitTracker Team",
+                        Url = new Uri(contactAdress)
+                    }
+                });
+            });
+
+            // Add JWT authentication to Swagger
+
+            return services;
+        }
+    }
+}
--- src/FitTracker.API/Program.cs
+++ src/FitTracker.API/Program.cs
@@ -1,1 +1,27 @@
+using FitTracker.API.Configurations;
 
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.ConfigureSwagger(builder.Configuration);
+const string corsPolicy = "_corsPolicy";
+
+
+var app =  builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseRouting();
+app.UseCors(corsPolicy);
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
