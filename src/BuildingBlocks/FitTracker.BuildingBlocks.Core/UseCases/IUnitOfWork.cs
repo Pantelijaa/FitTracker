82b56dd488a93289230e1f7a179d448e9ef902a2
Nikola <nikolapan53@gmail.com>
2026-08-24 12:09:28 +0200

    Base UoW

---
+++ src/BuildingBlocks/FitTracker.BuildingBlocks.Core/UseCases/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using FluentResults;
+
+namespace FitTracker.BuildingBlocks.Core.UseCases
+{
+    public interface IUnitOfWork
+    {
+        void BeginTransaction();
+        Result Save();
+        Result Commit();
+        Result Rollback();
+    }
+}
---
+++ src/BuildingBlocks/FitTracker.BuildingBlocks.Infrastrucutre/Database/UnitOfWork.cs
@@ -0,0 +1,61 @@
+﻿using FluentResults;
+using Microsoft.EntityFrameworkCore;
+using FitTracker.BuildingBlocks.Core.UseCases;
+using Microsoft.EntityFrameworkCore.Storage;
+
+
+
+
+namespace FitTracker.BuildingBlocks.Infrastrucutre.Database
+{
+    public class UnitOfWork<TDbContext> : IUnitOfWork where TDbContext : DbContext
+    {
+        private readonly TDbContext _dbContext;
+        private IDbContextTransaction? _transaction;
+
+        public UnitOfWork(TDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void BeginTransaction()
+        {
+            _transaction ??= _dbContext.Database.BeginTransaction();
+        }
+
+        public Result Save()
+        {
+            try
+            {
+                _dbContext.SaveChanges();
+                return Result.Ok();
+            }
+            catch (DbUpdateException ex)
+            {
+                return Result.Fail(ex.Message);
+            }
+        }
+
+        public Result Commit()
+        {
+            if (_transaction is null) 
+                return Result.Fail("No transaction to commit.");
+
+            _transaction.Commit();
+            _transaction.Dispose();
+            _transaction = null;
+            return Result.Ok();
+        }
+
+        public Result Rollback()
+        {
+            if ( _transaction is null)
+                return Result.Fail("No transaction to rollback.");
+
+            _transaction.Rollback();
+            _transaction.Dispose();
+            _transaction = null;
+            return Result.Ok();
+        }
+    }
+}
