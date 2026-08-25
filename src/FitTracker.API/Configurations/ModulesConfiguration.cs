0e2ac2472e5549a4a8130a52dc7f150265003f92
Nikola <nikolapan53@gmail.com>
2026-08-25 12:17:49 +0200

    Empty Module Configuration

---
+++ src/FitTracker.API/Configurations/ModulesConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using FitTracker.Workouts.Infrastructure;
+
+namespace FitTracker.API.Configurations
+{
+    public static class ModulesConfiguration
+    {
+        public static IServiceCollection ConfigureModules(this IServiceCollection services)
+        {
+            services.ConfigureWorkoutsModules();
+            return services;
+        }
+    }
+}
---
+++ src/Modules/Workouts/FitTracker.Workouts.Infrastructure/WorkoutsStartup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace FitTracker.Workouts.Infrastructure
+{
+    public static class WorkoutsStartup
+    {
+        public static IServiceCollection ConfigureWorkoutsModules(this IServiceCollection services)
+        {
+
+            SetupCore(services);
+            SetupInfrastucture(services);
+            return services;
+        }
+
+        private static void SetupCore(IServiceCollection services)
+        {
+            // Register core services here
+        }
+
+        private static void SetupInfrastucture(IServiceCollection services)
+        {
+            // Register infrastructure repositories here
+        }
+    }
+}
