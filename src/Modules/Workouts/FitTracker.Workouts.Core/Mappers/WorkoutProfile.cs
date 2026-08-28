7bb3d07b442349341700805713ca8d0f98b022eb
Nikola <nikolapan53@gmail.com>
2026-08-28 13:05:12 +0200

    Wired AutoMapper

---
+++ src/Modules/Workouts/FitTracker.Workouts.Core/Mappers/WorkoutProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using FitTracker.Workouts.API.Dtos;
+using FitTracker.Workouts.Core.Domain;
+
+namespace FitTracker.Workouts.Core.Mappers
+{
+    public class WorkoutProfile : Profile
+    {
+
+        public WorkoutProfile()
+        {
+            CreateMap<Workout, WorkoutDto>().ReverseMap();
+            CreateMap<Workout, WorkoutCalendarDto>().ReverseMap();
+
+            CreateMap<WorkoutExercise, WorkoutExerciseDto>().ReverseMap();
+
+            CreateMap<ExerciseSet, ExerciseSetDto>().ReverseMap();
+
+            CreateMap<ExerciseSetSnapshot, ExerciseSetSnapshotDto>().ReverseMap();
+        }
+    }
+}
